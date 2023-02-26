4bce0a935cbafea39776db1194642bdb79674e7e
bunssss <tkylecaluag26@gmail.com>
2023-02-26 12:12:19 -0800

    form completed

--- script.js
+++ script.js
@@ -2,6 +2,10 @@
 const tabs = document.querySelector("#tabs").children;
 const sections = document.querySelectorAll("section");
 
+// const gameCardTemplate = document.querySelector("[games-template]")
+// const gameCardContainer = document.querySelector("[data-games-cards-container]")
+
+
 // Add click event listeners to the tabs
 for (let i = 0; i < tabs.length; i++) {
   tabs[i].addEventListener("click", function() {
@@ -21,3 +25,15 @@ for (let i = 0; i < tabs.length; i++) {
 tabs[0].classList.add("active");
 sections[0].style.display = "block";
 
+// const card =  gameCardTemplate.content.cloneNode(true).children[0]
+//   const gname = card.querySelector("[game-name]")
+//   const date1 = card.querySelector("[date-started]")
+//   const date2 = card.querySelector("[date-completed]")
+//   const rating = card.querySelector("[rating]")
+//   const comments = card.querySelector("[comments]")
+//   gname.textContent = document.querySelector('#gamename')
+//   date1.textContent = document.querySelector('#DateStart')
+//   date2.textContent = document.querySelector('#DateComplete')
+//   rating.textContent = document.querySelector('#Rating')
+//   comments.textContent = document.querySelector('#Comments')
+//   gameCardContainer.append(card)
