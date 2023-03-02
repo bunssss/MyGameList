d7c4712d5eaf4bdf56d3bb0ca9814c44f575f50a
buns <tkylecaluag26@gmail.com>
2023-03-02 10:02:35 -0800

    update

--- apitesting.js
+++ apitesting.js
@@ -1,6 +1,7 @@
 // import * as dotenv from 'dotenv' // see https://github.com/motdotla/dotenv#how-do-i-use-dotenv-with-import
 // dotenv.config()
 
+const data = 'fields name; limit 10;'
 const input = 'Call%20of%20Duty'
 const url = ('https://api.igdb.com/v4/games/?fields=name,genres&search='+ input)
 const gameCardTemplate = document.querySelector("[games-template]")
@@ -10,6 +11,7 @@ const gameCardContainer = document.querySelector("[data-games-cards-container]")
 // const token = process.env.TOKEN
 
 fetch(url, {
+  method: 'POST',
   headers: {
     'Client-ID': '',
     'Authorization': '',
@@ -20,12 +22,32 @@ fetch(url, {
 .then(data => {
   data.forEach(gameItem => {
   const card =  gameCardTemplate.content.cloneNode(true).children[0]
-  const header = card.querySelector("[data-header]")
-  const summary = card.querySelector("[data-genre]")
+  const header = card.querySelector("[data-gname]")
+  const summary = card.querySelector("[data-summary]")
   header.textContent = gameItem.name
-  summary.textContent = gameItem.storyline
+  summary.textContent = "summary"
   gameCardContainer.append(card)
   console.log(card)
   })
 })
-.catch(error => console.log('ERROR'))
+.catch(error => console.log(error))
+
+// const data = 'fields name; limit 10;'
+// const input = 'Call%20of%20Duty'
+// const url = ('https://api.igdb.com/v4/games/?fields=name,genres&search='+ input)
+
+// const ClientID = process.env.CLIENTID
+// const token = process.env.TOKEN
+
+// fetch(url, {
+//   method: 'POST',
+//   headers: {
+//     'Client-ID': 'bgiba2kpjhy98ea8s5xu3d04sx72kn',
+//     'Authorization': 'Bearer 371rntzw90vdgqrad4od50sgrs6st3',
+//     'Content-Type': 'application/json'
+//   },
+//   body: JSON.stringify(data)
+
+// }).then(res =>  res.json())
+// .then(data => console.log(data))
+// .catch(error => console.log('ERROR'))
